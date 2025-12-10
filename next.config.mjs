4705ae5bac85ef2e1cbf1d4d9181f31b55f2bc7a
Mailson Rafael <mailson.institutosementes@gmail.com>
2025-12-10 00:12:50 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
-import type {NextConfig} from 'next';
 import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
